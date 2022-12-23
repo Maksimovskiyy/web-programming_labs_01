22c6026c496f007c9c2321eda309fd557805c3a2
AfGAY <movchinnikov00@mail.ru>
2022-12-23 10:46:34 +0700

    Переключения состояний

--- lab9.js
+++ lab9.js
@@ -1,12 +1,30 @@
-function clickMe(buttont) {
-    if(buttont.innerHTML == 'Щелчок')
-        buttont.innerHTML = 'Спасибо';
+function clickMe(button) {
+    if(button.innerHTML == 'Щелчок')
+        button.innerHTML = 'Спасибо';
     else
-        buttont.innerHTML = 'Щелчок';
+        button.innerHTML = 'Щелчок';
 }
-function clickName(buttont) {
-    if(buttont.innerHTML == 'Максим')
-        buttont.innerHTML = 'Овчинников';
+function clickName(button) {
+    if(button.innerHTML == 'Максим')
+        button.innerHTML = 'Овчинников';
     else
-        buttont.innerHTML = 'Максим';
+        button.innerHTML = 'Максим';
+}
+function clickMeAagain(button) {
+    if(button.innerHTML == 'Нажми меня')
+        button.innerHTML = 'Ещё раз нажми';
+    else if(button.innerHTML == 'Ещё раз нажми')
+        button.innerHTML = 'Ещё';
+    else if(button.innerHTML == 'Ещё')
+        button.innerHTML = 'Начать сначала';
+    else if(button.innerHTML == 'Начать сначала')
+        button.innerHTML = 'Нажми меня';
+    else
+        button.innerHTML = 'Нажми меня';
+
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('4').style.backgroundColor = color
 }
