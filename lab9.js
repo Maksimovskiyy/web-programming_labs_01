29b5b38c02592836a6236b0dcb7887deedaab975
AfGAY <movchinnikov00@mail.ru>
2022-12-23 11:07:56 +0700

    Вывод координат мыши

--- lab9.js
+++ lab9.js
@@ -28,3 +28,25 @@ function clickMeAagain(button) {
     let color = 'rgb(' + r + ',' + g + ',' + b + ')';
     document.getElementById('4').style.backgroundColor = color
 }
+
+function showCoords(event) {
+    event.target.innerHTML = 'x = ' + event.offsetX + ', y = ' + event.offsetY;
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    let coords = document.querySelector('#coords');
+    coords.addEventListener('mousemove', showCoords);
+
+    coords.addEventListener('click', func);
+    function func(event) {
+        if (event.ctrlKey) {
+            alert('x = ' + event.offsetX + ', y = ' + event.offsetY + ' и нажат Ctrl');
+        }
+        if (event.altKey) {
+            alert('x = ' + event.offsetX + ', y = ' + event.offsetY + ' и нажат Alt');
+        }
+        if (event.shiftKey) {
+            alert('x = ' + event.offsetX + ', y = ' + event.offsetY + ' и нажат Shift');
+        }
+    }
+});
